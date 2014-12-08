aa055da0214cbc8b9fade01fbbb07026ecb6da0e
LOZORD <ljrudberg@gmail.com>
2014-12-08 13:07:19 -0500

    Updated Gruntfile to accept port options

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,10 +66,10 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: grunt.option('port') || 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35729
+        livereload: grunt.option('livereload') || 35729
       },
       livereload: {
         options: {
@@ -88,7 +88,7 @@ module.exports = function (grunt) {
       },
       test: {
         options: {
-          port: 9001,
+          port: grunt.option('port') || 9001,
           middleware: function (connect) {
             return [
               connect.static('.tmp'),
