0f2ec0d7436d9c0255d62d49946cf79c55becf6c
LOZORD <ljrudberg@gmail.com>
2014-12-08 13:11:51 -0500

    Changed Gruntfile from localhost to 0.0.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
       options: {
         port: grunt.option('port') || 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: grunt.option('livereload') || 35729
       },
       livereload: {
