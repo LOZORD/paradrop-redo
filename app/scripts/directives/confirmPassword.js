d933d8b0c784e0888596ac8dd1dcd69645d507c1
LOZORD <ljrudberg@gmail.com>
2014-12-29 15:59:58 -0500

    Another attempt at password confirmation. Giving up for now :'(

--- app/scripts/directives/confirmPassword.js
+++ app/scripts/directives/confirmPassword.js
@@ -1,26 +1,19 @@
-/* FOUND HERE: https://gist.github.com/sahat/0f4c915793458753397b */
 'use strict';
 
 angular.module('paradropApp')
   .directive('confirmPassword', function () {
     return {
       require:  'ngModel',
-      link:     function (scope, elem, attrs, ctrl) {
-        var otherInput = elem.inheritedData('$formController')[attrs.confirmPassword];
+      scope: {
+        otherModelValue: '=confirmPassword'
+      },
+      link: function(scope, elem, attrs, ngModel) {
+        ngModel.$validators.confirmPassword = function(modelValue) {
+          return modelValue === scope.otherModelValue;
+        };
 
-        ctrl.$parsers.push(function (value) {
-          if (value === otherInput.$viewValue) {
-            ctrl.$setValidity('repeat', true);
-            return value;
-          }
-          else {
-            ctrl.$setValidity('repeat', false);
-          }
-        }); 
-
-        otherInput.$parsers.push(function (value) {
-          ctrl.$setValidity('repeat', value == ctrl.$viewValue);
-          return value;
+        scope.$watch('otherModelValue', function () {
+          ngModel.$validate();
         });
       }
     };
