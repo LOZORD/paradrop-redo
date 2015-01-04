b4c68c8f867e3cfb76b249a58a88aada17e884e0
LOZORD <ljrudberg@gmail.com>
2015-01-03 19:40:09 -0500

    More work with blogs

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -8,10 +8,25 @@
  * Controller of the paradropApp
  */
 angular.module('paradropApp')
-  .controller('BlogCtrl', ['$scope', '$location', '$http', '$routeParams',
-    function ($scope, $location, $http, $routeParams) {
-      $scope.name = 'Leo';
-      $scope.x = 11;
-      $scope.y = 3;
+  .controller('BlogCtrl', ['$scope', '$location', '$http', '$routeParams', 'URLS',
+    function ($scope, $location, $http, $routeParams, URLS) {
+      var ts = null;
+      $scope.posts = [];
+
+      var blogUrl = URLS.https + '/blog/';
+      var payload = {};
+
+      if ($routeParams.ts) {
+        payload.ts = parseInt($routeParams.ts, 10);
+      }
+
+      //TODO if given a range or something...
+
+      $http.get(blogUrl, payload)
+        .success(
+          function(data) {
+            $scope.posts = data;
+          }
+      );
     }
   ]);
