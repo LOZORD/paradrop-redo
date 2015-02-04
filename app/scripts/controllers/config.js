3d39f1ea44d92572b5c0d20382d8f2f1c760ab80
LOZORD <ljrudberg@gmail.com>
2015-02-04 14:11:44 -0500

    Working more on the view, added alert for slow channel choice

--- app/scripts/controllers/config.js
+++ app/scripts/controllers/config.js
@@ -16,24 +16,19 @@ angular.module('paradropApp')
         $scope.configurableDevices = $scope.currentUser.aps.filter(function (device) {
           return device.type.toLowerCase() === 'owner';
         });
-        //sort the devices by name
-        $scope.configurableDevices.sort(function (a,b) {
-          if (a.name > b.name) {
-            return 1;
-          }
-          else if (a.name < b.name) {
-            return -1;
-          }
-          else {
-            return 0;
-          }
-        });
-        //console.log($scope.configurableDevices);
-
         //if we were routed here for updating a config
         if ($routeParams.cDeviceID) {
+
           $scope.deviceToUpdate = null;
+
           $scope.CHANNELS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 36, 40, 44, 48, 149, 153, 157, 161, 165];
+
+          //TODO, use http to get default values
+          $scope.configUpdateData = {
+            mode: 'auto',
+            channel: 1
+          };
+
           for (var i = 0, len = $scope.configurableDevices.length; i < len; i++) {
             if ($scope.configurableDevices[i].guid === $routeParams.cDeviceID) {
               $scope.deviceToUpdate = $scope.configurableDevices[i];
@@ -41,11 +36,23 @@ angular.module('paradropApp')
             }
           }
 
-          console.log($scope.deviceToUpdate);
-
           $scope.submitUpdate = function (data, isValid) {
             if (!isValid) {
               return;
+            }
+          };
+
+          $scope.usingSlowChannel = function (val) {
+            console.log(val);
+            var contains =  $scope.CHANNELS.filter(function (i) {
+                              return i <= 11;
+                            }).indexOf(val) !== -1;
+
+            if (contains) {
+              return [1, 6, 11].indexOf(val) === -1;
+            }
+            else {
+              return false;
             }
           };
         }
