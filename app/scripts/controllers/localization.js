8cc2c1ddb7ac40355bc466715394ad67575f55e7
Nick Hyatt <nhyatt@wisc.edu>
2015-02-18 16:59:36 -0500

    changing icon localization

--- app/scripts/controllers/localization.js
+++ app/scripts/controllers/localization.js
@@ -14,31 +14,6 @@ angular.module('paradropApp')
       $scope.authorizePage()
       .then(function(authorized){
         if(authorized){
-          //default values
-          $scope.showMarkers = true;
-          $scope.markerBtnText = 'Hide';
-          $scope.heatBtnText = 'Hide';
-          //change toggle btn text
-          $scope.changeText = function(hidden, markerBtn){
-            if(!hidden){
-              if(markerBtn){
-                $scope.markerBtnText = 'Hide';
-              }else{
-                $scope.heatBtnText = 'Hide';
-              }
-            }else{
-              if(markerBtn){
-                $scope.markerBtnText = 'Show';
-              }else{
-                $scope.heatBtnText = 'Show';
-              }
-            }
-          };
-          //create a mapping
-          $scope.apNameMap = {};
-          for(var ap in $scope.currentUser().aps){
-            $scope.apNameMap[$scope.currentUser().aps[ap].guid] = $scope.currentUser().aps[ap].name;
-          }
 
           //grab map and build!
           var body = { sessionToken: $scope.sessionToken() };
@@ -46,20 +21,7 @@ angular.module('paradropApp')
           $http.post(localURL, body ).then(
               function(json){
                 $scope.array = json.data;
-                $scope.heatMapData = [];
-                for(var point in $scope.array[0].heatmap){
-                  $scope.heatMapData
-                    .push(
-                      {
-                        location: new google.maps
-                          .LatLng($scope.array[0].heatmap[point][0],
-                                  $scope.array[0].heatmap[point][1]),
-                        weight: $scope.array[0].heatmap[point][2]
-                      }
-                    );
-                }
-                $scope.real = {lat: $scope.array[0].true_coord[0], lng: $scope.array[i].true_coord[1]};
-                $scope.pred = {lat: $scope.array[0].pred_coord[0], lng: $scope.array[i].pred_coord[1]};
+                $scope.buildHeatMap();
               }
           ).then(function(){
             var mapInitURL = URLS.current + 'recon/maps/init';
@@ -69,17 +31,19 @@ angular.module('paradropApp')
                   $scope.groupMaps = groupMaps.data[0];
                   $scope.mapData = $scope.groupMaps.data;
                   var builtMap = gmapMaker.buildMap($scope.mapData);
-                  var builtMap = gmapMaker.buildMap($scope.mapData);
+                  $scope.apNameMap = $scope.groupMaps.map;
+                  $scope.revApNameMap = {};
+                  for(var key in $scope.apNameMap){
+                    $scope.revApNameMap[$scope.apNameMap[key].apid] = $scope.apNameMap[key].name;
+                  }
                   $scope.firstFloorMapType = builtMap.mapType;
                   $scope.onClick = builtMap.onClick;
                   $scope.$on('mapInitialized', function(event, map) {
                     $scope.heatmap = map.heatmapLayers.foo;
                     $scope.map = map;
                     $scope.heatmap.set('radius', 40);
+                    setTimeout(function(){$scope.changeMarkers();}, 1000);
                   });
-                  $scope.toggleHeatmap= function(event) {
-                    $scope.heatmap.setMap($scope.heatmap.getMap() ? null : $scope.map);
-                  };
                   $scope.mapReady = true;
                 }
             , function(error){$scope.mapError = true;});
@@ -92,16 +56,46 @@ angular.module('paradropApp')
         if(i < 0){
           i = $scope.array.length -1;
         }
+        $scope.changeMarkers();
+        $scope.buildHeatMap();
         $scope.setHeatMap();
       }
+
+      $scope.changeMarkers = function(){
+        for(var key in $scope.map.markers){
+          if(key === 'actualLocation' || key === 'predictedLocation'){
+            //Nothing
+          }else{
+            if($scope.array[i].rssi[key]){
+              var strength = $scope.array[i].rssi[key];
+              if(strength > -65){
+                $scope.map.markers[key].set('visible', true);
+                $scope.map.markers[key].setIcon('images/green-signal.png');
+              }else if(strength > -85){
+                $scope.map.markers[key].set('visible', true);
+                $scope.map.markers[key].setIcon('images/yellow-signal.png');
+              }else{
+                $scope.map.markers[key].set('visible', true);
+                $scope.map.markers[key].setIcon('images/red-signal.png');
+              }
+            }else{
+                $scope.map.markers[key].set('visible', false);
+            }
+          }
+        }
+      }
+
       $scope.nextPoint = function(){
         i++;
         if(i >= $scope.array.length){
           i = 0;
         }
+        $scope.changeMarkers();
+        $scope.buildHeatMap();
         $scope.setHeatMap();
       }
-      $scope.setHeatMap = function(){
+
+      $scope.buildHeatMap = function(){
         $scope.heatMapData = [];
         for(var point in $scope.array[i].heatmap){
           $scope.heatMapData
@@ -114,11 +108,14 @@ angular.module('paradropApp')
               }
             );
         }
-        if(i > 0){
-        }
         $scope.real = {lat: $scope.array[i].true_coord[0], lng: $scope.array[i].true_coord[1]};
         $scope.pred = {lat: $scope.array[i].pred_coord[0], lng: $scope.array[i].pred_coord[1]};
-        $scope.heatmap.set('data', $scope.heatMapData);
+      }
+
+      $scope.setHeatMap = function(){
+        $scope.heatmap.setMap(null);
+        $scope.heatmap = new google.maps.visualization.HeatmapLayer({radius: 40, data: $scope.heatMapData});
+        $scope.heatmap.setMap($scope.map);
       }
       });
 }]);
