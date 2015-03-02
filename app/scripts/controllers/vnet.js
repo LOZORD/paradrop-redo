f61a91cc1f499eed39fe9add2ee2ed5a2d1c3d1f
LOZORD <ljrudberg@gmail.com>
2015-03-02 13:37:39 -0500

    More work with vnet UI and logic

--- app/scripts/controllers/vnet.js
+++ app/scripts/controllers/vnet.js
@@ -13,8 +13,46 @@ angular.module('paradropApp')
     $scope.authorizePage()
     .then(function(isAuthorized) {
       if (isAuthorized) {
-        //TODO
         $scope.togglerBtn = false;
+        $scope.vnetToUpdate = $scope.specificChute;
+
+        //FIXME in the view
+        $scope.ENCRYPTION_TYPES = [
+          'None',
+          'PSK',
+          'PSK2'
+        ];
+
+        $scope.vnetUpdateData = {
+          ssid:       null,
+          password:   null,
+          encryption: null,
+          subnet:     null,
+          radioid:    null,
+          isprimary:  null,
+          qosup:      null,
+          qosdn:      null
+        };
+
+        var vnetFetchDataPayload = {
+          sessionToken: $scope.currentUser().id,
+          chuteid:      $routeParams.chuteid
+        };
+
+        var vnetFetchDataURL = URLS.current + 'ap/chute/network';
+
+        console.log('fetching vnet data',vnetFetchDataPayload);
+
+        $http.post(vnetFetchDataURL, vnetFetchDataPayload)
+        .then(
+          function (result) {
+            console.log('got result!');
+            console.log(result);
+          },
+          function (result) {
+            console.log(result);
+          }
+        );
       }
     });
   }]);
